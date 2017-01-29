08b06e66bfb4c2dddbe3d9c5bfe4cbb113b9e42e
Toby Tianxiao Zhao <tzh@kth.se>
2017-01-29 20:25:40 +0100

    Add files via upload

---
+++ Images-mat/Contents.m
@@ -0,0 +1,30 @@
+% Image library 2D1420 Computer Vision
+%
+% These images stored on a binary format (not guaranteed to be
+% portable). The number in the name indicates the image size. 
+% To read one of these images you execute a Matlab-command like
+%
+% >> load autumn.mat
+%
+% after which the image will appear as a variable in the list
+% that appears when you execute the command "whos". (In this
+% case, the variable is named "X".)
+%
+% Available images:
+%
+%   autumn.mat
+%   blocks256.mat
+%   borabora.mat
+%   canoe.mat
+%   canoe256.mat
+%   circbw.mat
+%   circuit.mat
+%   kids.mat
+%   logo.mat
+%   pout.mat
+%   rgbtrees.mat
+%   roishadow.mat
+%   shadow.mat
+%   shadow256.mat
+%   tire.mat
+%   tools256.mat
