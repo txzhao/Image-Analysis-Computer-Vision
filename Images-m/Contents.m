30e6a1487c2c282bfcd0009982f8b53afb5e1e43
Toby Tianxiao Zhao <tzh@kth.se>
2017-01-29 20:24:01 +0100

    Add files via upload

---
+++ Images-m/Contents.m
@@ -0,0 +1,58 @@
+% Image library 2D1420 Computer Vision
+%
+% These images stored as Matlab functions (on a portable ascii format).
+% The number in the name indicates the image size. To read one of
+% these images you execute a Matlab-command like
+%
+% >> mypic = cern128
+%
+% Available images:
+%
+%   cern128.m
+%   cern256.m
+%   few128.m
+%   few256.m
+%   few512.m
+%   genevepark128.m
+%   genevepark256.m
+%   godthem128.m
+%   godthem256.m
+%   godthem512.m
+%   hand128.m
+%   hand256.m
+%   houghtest256.m
+%   kaffe128.m
+%   kaffe256.m
+%   kallskarskannan128.m
+%   kallskarskannan256.m
+%   kloss128.m
+%   kloss256.m
+%   lesshard256.m
+%   male128.m
+%   male256.m
+%   nallo128.m
+%   nallo256.m
+%   nallo256float.m
+%   nallo256what.m
+%   nallo512.m
+%   notquiteasy256.m
+%   perspwave128.m
+%   perspwave256.m
+%   phonecalc128.m
+%   phonecalc256.m
+%   phonecalc512.m
+%   suburb128.m
+%   suburb256.m
+%   suburbleft128.m
+%   suburbleft256.m
+%   sunflower128.m
+%   sunflower256.m
+%   tower128.m
+%   tower256.m
+%   triangle128.m
+%   triangle64.m
+%   vapzoom128.m
+%   vapzoom256.m
+%   vistasbackpack128.m
+%   vistasbackpack256.m
+%   whatisthis256.m
