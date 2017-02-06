bad66d9d04a8a288109927c917c8220ebf47a696
Toby Tianxiao Zhao <tzh@kth.se>
2017-02-06 16:33:48 +0100

    Update extractedge.m

--- edge-detection-hough/script/extractedge.m
+++ edge-detection-hough/script/extractedge.m
@@ -9,7 +9,7 @@ Lvvv = Lvvvtilde(discgaussfft(inpic, scale), shape);
 Lv_mask = (lv > threshold) - 0.5;
 Lvvv_mask = (Lvvv < 0) - 0.5;
 
-% keep zerocrossings with negative Lvvv and Lv above threshold
+% sort zerocrossings with negative Lvvv and Lv above threshold
 curves = zerocrosscurves(Lvv, Lvvv_mask);
 curves = thresholdcurves(curves, Lv_mask);
 overlaycurves(inpic, curves);
